3a18fde310a3bd73741c32d49231928cc3833d9e
Kiss David <deco1996@mailbox.hu>
2018-11-04 14:40:23 +0100

    initial commit 2

---
+++ src/main/java/com/example/springbootpizzaqueen/SpringbootPizzaqueenApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootpizzaqueen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPizzaqueenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPizzaqueenApplication.class, args);
+	}
+}
