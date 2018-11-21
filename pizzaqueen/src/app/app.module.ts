c2dfa34371749b7593f1834ccfc30aecdd194ee2
qexer <qexer7@gmail.com>
2018-11-21 11:51:23 +0100

    site skeleton

--- pizzaqueen/src/app/app.module.ts
+++ pizzaqueen/src/app/app.module.ts
@@ -8,10 +8,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    MenuComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     FlexLayoutModule,
